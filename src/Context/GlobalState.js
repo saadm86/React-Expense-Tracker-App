652e1ad45662793b5afdfcf33a6d3ebb2f6460c0
Saad Muhamamd <s.muhammad86@gmail.com>
2020-09-30 16:25:28 +0500

    Updated Global State

--- src/Context/GlobalState.js
+++ src/Context/GlobalState.js
@@ -1,15 +1,13 @@
 import React, { createContext, useState } from 'react'
 
 const transactions = [
-    {amount: 500, desc: "Cash"},
-    {amount: -40, desc: "Book"},
-    {amount: -200, desc: "Camera"}
+    
   ]
   
 export const TransactionsContext = createContext(transactions);
 
 const Transprovider = ({children}) => {
-    
+
     let [text, setText] = useState("")
     let [amount, setAmount] = useState("")
     let [trans, setTrans] = useState(transactions)
